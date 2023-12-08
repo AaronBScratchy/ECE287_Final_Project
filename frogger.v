832d96a9ba8c10cd667154c1296d831b599d2121
AaronBScratchy <aarondanielbenjamin@gmail.com>
2023-12-08 15:47:56 -0500

    Attempt for enemy movement not great

--- frogger.v
+++ frogger.v
@@ -21,6 +21,8 @@ wire clk_25;
 wire clk_5;
 Clock_divider clk_25_MHz(clock, clk_25);
 
+reg [31:0]count;
+
 
 
 vga_driver driver(clk_25, rev_reset, color_in, next_x, next_y, hsync, vsync, red, green, blue, sync, clk, blank);
@@ -41,27 +43,27 @@ parameter DONE = 4'b1001;
 
 reg [3:0]s;
 reg [3:0]ns;
+parameter E1M1 = 5'd0;
+parameter E1M2 = 5'd1;
+parameter E1M3 = 5'd2;
+parameter E1M4 = 5'd3;
+parameter E1M5 = 5'd4;
+parameter E1M6 = 5'd5;
+parameter E1M7 = 5'd6;
+parameter E1M8 = 5'd7;
+parameter E1M9 = 5'd8;
+parameter E1M10 =5'd9;
+parameter E1M11 = 5'd10;
+parameter E1M12 = 5'd11;
+parameter E1M13 = 5'd12;
+parameter E1M14 = 5'd13;
+parameter E1M15 = 5'd14;
+parameter E1M16 = 5'd15;
+parameter E1M17 = 5'd16;
+parameter E1M18 = 5'd17;
+parameter E1M19 = 5'd18;
+parameter E1M20 = 5'd19;
 
-parameter E1M1 = 5'd1;
-parameter E1M2 = 5'd2;
-parameter E1M3 = 5'd3;
-parameter E1M4 = 5'd4;
-parameter E1M5 = 5'd5;
-parameter E1M6 = 5'd6;
-parameter E1M7 = 5'd7;
-parameter E1M8 = 5'd8;
-parameter E1M9 = 5'd9;
-parameter E1M10 = 5'd10;
-parameter E1M11 = 5'd11;
-parameter E1M12 = 5'd12;
-parameter E1M13 = 5'd13;
-parameter E1M14 = 5'd14;
-parameter E1M15 = 5'd15;
-parameter E1M16 = 5'd16;
-parameter E1M17 = 5'd17;
-parameter E1M18 = 5'd18;
-parameter E1M19 = 5'd19;
-parameter E1M20 = 5'd20;
 
 reg [5:0]E1s;
 reg [5:0]E1ns;
@@ -92,21 +94,8 @@ module vga_driver (
 
 
 
-/*always @(posedge clk_25 or negedge rev_reset) begin
-	if(rev_reset == 1'b0) begin
-		if(SW[0] == 1'b1) begin
-			if(next_y < 10'd96)
-				color_in <= 8'b00011100;
-			else
-				color_in <= 8'b00000000;
-		end else
-			color_in <= 8'b00000000;
-	end else
-		color_in <= 8'b00000000;
-			
-end*/
 //Player FSM
-always@(*) begin
+/*always@(*) begin
 case(s)
 	START: begin
 				if(SW[17] == 1'd1)
@@ -165,105 +154,173 @@ case(s)
 	DONE: ns = DONE;
 	default: ns = START;
 endcase
-end
+end*/
 
-/*always@(*) begin
+always@(*) begin
 		case(E1s)
-			E1M1: E1ns <= E1M2;
-			E1M2: E1ns <= E1M3;
-			E1M3: E1ns <= E1M4;
-			E1M4: E1ns <= E1M5;
-			E1M5: E1ns <= E1M6;
-			E1M6: E1ns <= E1M7;
-			E1M7: E1ns <= E1M8;
-			E1M8: E1ns <= E1M9;
-			E1M9: E1ns <= E1M10;
-			E1M10: E1ns <= E1M11;
-			E1M11: E1ns <= E1M12;
-			E1M12: E1ns <= E1M13;
-			E1M13: E1ns <= E1M14;
-			E1M14: E1ns <= E1M15;
-			E1M15: E1ns <= E1M16;
-			E1M16: E1ns <= E1M17;
-			E1M17: E1ns <= E1M18;
-			E1M18: E1ns <= E1M19;
-			E1M19: E1ns <= E1M20;
-			E1M20: E1ns <= E1M1;
-			default: E1ns <= E1M1;
+			E1M1: E1ns = E1M2;
+			E1M2: E1ns = E1M3;
+			E1M3: E1ns = E1M4;
+			E1M4: E1ns = E1M5;
+			E1M5: E1ns = E1M6;
+			E1M6: E1ns = E1M7;
+			E1M7: E1ns = E1M8;
+			E1M8: E1ns = E1M9;
+			E1M9: E1ns = E1M10;
+			E1M10: E1ns = E1M11;
+			E1M11: E1ns = E1M12;
+			E1M12: E1ns = E1M13;
+			E1M13: E1ns = E1M14;
+			E1M14: E1ns = E1M15;
+			E1M15: E1ns = E1M16;
+			E1M16: E1ns = E1M17;
+			E1M17: E1ns = E1M18;
+			E1M18: E1ns = E1M19;
+			E1M19: E1ns = E1M20;
+			E1M20: E1ns = E1M1;
+			default: E1ns = E1M1;
 		endcase
-end*/
-
-/*always@(posedge clock or negedge rev_reset) begin
-if(rev_reset == 1'b0)begin
-	case(s)
-		E1M1: begin
-			E1_x <= 10'd32;
-		end
-		E1M2: begin
-			E1_x <= 10'd64;
-		end
-		E1M3: begin
-			E1_x <= 10'd96;
-		end
-		E1M4: begin
-			E1_x = 10'd128;
-		end
-		E1M5: begin
-			E1_x <= 10'd160;
-		end
-		E1M6: begin
-			E1_x <= 10'd192;
-		end
-		E1M7: begin
-			E1_x <= 10'd224;
-		end
-		E1M8: begin
-			E1_x <= 10'd256;
-		end
-		E1M9: begin
-			E1_x <= 10'd288;
-		end
-		E1M10:begin
-			E1_x <= 10'd320;
-		end
-		E1M11:begin
-			E1_x <= 10'd352;
-		end
-		E1M12:begin
-			E1_x <= 10'd384;
-		end
-		E1M13:begin
-			E1_x <= 10'd416;
-		end
-		E1M14:begin
-			E1_x <= 10'd448;
-		end
-		E1M15:begin
-			E1_x <= 10'd480;
-		end
-		E1M16:begin
-			E1_x <= 10'd512;
-		end
-		E1M17:begin
-			E1_x <= 10'd544;
-		end
-		E1M18:begin
-			E1_x <= 10'd576;
-		end
-		E1M19:begin
-			E1_x <= 10'd608;
-		end
-		E1M20:begin
-			E1_x <= 10'd640;
-		end
-		default: E1_x <= 10'd0;
-	endcase
-end else
-	E1_x <= 10'd0;
-end*/
+end
 
 
 always@(posedge clock or negedge rev_reset) begin
+if(rev_reset == 1'b0)begin
+	case(E1s)
+		E1M1: begin
+			if(next_x < 10'd32 & next_x > 10'd0)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M2: begin
+			if(next_x < 10'd64 & next_x > 10'd32)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M3: begin
+			if(next_x < 10'd96 & next_x > 10'd64)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M4: begin
+			if(next_x < 10'd128 & next_x > 10'd96)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M5: begin
+			if(next_x < 10'd160 & next_x > 10'd128)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M6: begin
+			if(next_x < 10'd192 & next_x > 10'd160)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M7: begin
+			if(next_x < 10'd224 & next_x > 10'd192)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M8: begin
+			if(next_x < 10'd256 & next_x > 10'd224)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M9: begin
+			if(next_x < 10'd288 & next_x > 10'd256)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M10: begin
+			if(next_x < 10'd320 & next_x > 10'd288)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M11: begin
+			if(next_x < 10'd352 & next_x > 10'd320)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M12: begin
+			if(next_x < 10'd384 & next_x > 10'd352)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M13: begin
+			if(next_x < 10'd416 & next_x > 10'd384)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M14: begin
+			if(next_x < 10'd448 & next_x > 10'd416)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M15: begin
+			if(next_x < 10'd480 & next_x > 10'd448)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M16: begin
+			if(next_x < 10'd512 & next_x > 10'd480)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M17: begin
+			if(next_x < 10'd544 & next_x > 10'd512)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M18: begin
+			if(next_x < 10'd576 & next_x > 10'd544)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M19: begin
+			if(next_x < 10'd608 & next_x > 10'd576)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		E1M20: begin
+			if(next_x < 10'd640 & next_x > 10'd608)
+				color_in <= 8'b00011100;
+			else
+				color_in <= 8'b00000000;
+			end
+		default: begin
+			E1_x <= 10'd0;
+			color_in <= 8'd0;
+		end
+	endcase
+end else begin
+	E1_x <= 10'd0;
+	color_in <= 8'd0;
+	end
+end
+
+
+
+
+/*always@(posedge clock or negedge rev_reset) begin
 	if(rev_reset == 1'b0) begin
 		case(s)
 			START:begin
@@ -331,22 +388,30 @@ always@(posedge clock or negedge rev_reset) begin
 	end else begin
 		color_in <= 8'b00000000;
 	end
-end
+end*/
 
 
-always@(posedge clock or negedge rev_reset) begin
+/*always@(posedge clock or negedge rev_reset) begin
 	if(rev_reset == 1'b0) begin
 		s <= ns;
 	end else begin
 		s <= START;
 	end
-end
+end*/
 
-/*always@(posedge clock or negedge rev_reset)
+always@(posedge clock or negedge rev_reset) begin
 	if(rev_reset == 1'b0) begin
-		E1s <= E1ns;
+		if(count < 32'd50000000) begin
+			count <= count + 32'd1;
+			E1s <= E1s;
+		end else begin
+			count <= 32'd0;
+			E1s <= E1ns;
+		end
 	end else begin
 		E1s <= E1M1;
-	end*/
+		count <= 32'd0;
+	end
+end
 
 endmodule
